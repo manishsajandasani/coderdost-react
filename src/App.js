778c0eb2d4522c0f911e145a76b58cde28cfd5b6
Manish Sajandasani <msajandasani@gmail.com>
2023-09-22 20:01:34 +0530

    First Few Changes Here and There

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 
 function App() {
+  let name = "My First React Application";
+  let appHeaderClass = "App-header";
+  console.log("App Component");
   return (
     <div className="App">
-      <div className='App-header'>
-        Hello World
+      <div className={appHeaderClass}>
+        {/* This is React Expression */}
+        {name}
       </div>
+
+      {/* Use of Demo Component */}
+      <Demo />
+    </div>
+  );
+}
+
+// This is also a component and can be used as an HTML Tag. Use it in App() Component.
+function Demo() {
+  console.log("Demo Component");
+  return (
+    <div className='App-header'>
+      Demo
     </div>
   );
 }
